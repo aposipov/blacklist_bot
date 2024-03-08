3c5654e0177da10d7c3ff7ec7f499e17c1d5f169
aposipov <a.p.osipov@ro.ru>
2024-03-08 23:55:58 +1000

    menu blacklist

--- core_bot/handlers/menu_blacklist_handler.py
+++ core_bot/handlers/menu_blacklist_handler.py
@@ -10,6 +10,24 @@ from aiogram.fsm.context import FSMContext
 router = Router()
 
 
+class Profile(StatesGroup):
+	fill_fullname = State()
+	fill_bd = State()
+	fill_driver_id = State()
+	fill_img = State()
+	fill_comment = State()
+
+
 @router.callback_query(F.data == "add_blacklist")
-async def register(callback: CallbackQuery):
-	await callback.message.answer(text="You press ADD Blacklist")
+async def add_blacklist(callback: CallbackQuery, state: FSMContext):
+	await callback.message.answer(text="Введите полное ФИО водителя:")
+	await state.set_state(Profile.fill_fullname)
+
+
+@router.message(Profile.fill_fullname)
+async def fsm_fullname(message: Message, state: FSMContext):
+	await state.update_data(fullname=message.text)
+	await message.answer(text="Введите дату рождения")
+	await state.set_state(Profile.fill_bd)
+
+
